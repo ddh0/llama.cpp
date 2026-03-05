7ff8ec6d87f4efb0a2b89745be1a0ce6ca9ddc1d
ddh0 <chemist-mulches-39@icloud.com>
2026-03-05 00:47:42 -0600

    WIP

--- src/llama-quant-scheduler.cpp
+++ src/llama-quant-scheduler.cpp
@@ -1,23 +1,19 @@
-/* llama-quant-scheduler.cpp -- C++17
-
-ASPIRATIONS
------------
-
-Whenever possible, we must overlap computation and disk I/O. In fact, disk I/O is the main
-bottleneck in very many cases, and currently on `master` it's not handled very well - computation
-never overlaps with I/O. There is a great opportunity to improve it!
-
-At the time of writing (2026-03-02), the code on `master` is kept simple (if a bit messy) and it
-simply does...
-
-    load src data -> (convert to f32) -> quantize to target type -> write tensor data
-
-...in a for loop over all tensors. I believe we may be able to acheive a speedup of ~4x in _some_
-cases by managing the work to be done more effectively. There are many people quantizing many models
-every day with untold billions of parameters - we don't want to leave any performance on the table.
-
-The quantized tensors MUST end up in order in the output GGUF.
-*/
+/**
+ *
+ * Whenever possible, we aim to overlap computation and tensor data disk I/O.
+ *
+ * This is the primary bottleneck in very many cases, and currently it's not handled very
+ * efficiently - computation never overlaps with I/O on `master` at the time of writing. Rather,
+ * the code basically does:
+ *
+ *    load src tensor data -> dequantize and/or quantize -> write tensor data
+ *
+ * ...in a loop over all tensors. There is a great opportunity to improve it! I believe we may be
+ * able to acheive a speedup of ~4x in _some_ cases by overlapping the work to be done. There are
+ * many users quantizing many models with many billions of parameters - we don't want to leave any
+ * performance on the table.
+ *
+**/
 
 // #include "ggml-quants.h"
 #include "llama.h"
@@ -44,50 +40,46 @@ struct compute_pool {
         // TODO: init `busy` atomic flag?
     };
 
-    void start() {
-        // TODO: start the threads (but wait for work, don't spin)
-    };
-
-    void stop() {
-        // TODO: forcibly stop the thread pool (called should check `busy` before doing this)
+    bool distribute(tensor_sched_data & data) {
+        // TODO: distribute 
     };
 };
 
 //
 // quantization work scheduler
 //
-// goal: overlap I/O and computation, keep all threads busy (as much as reasonably possible)
+// goal: overlap I/O and computation as often as possible to speed-up the quantization process.
 //
-// the scheduler actually manages (`n_threads` + 2) threads:
+// the scheduler manages (`n_threads` + 2) threads:
 // - 1 thread for the `read_worker`
-// - `n_thread` threads for the `thread_pool` (tensor math is divided among compute workers)
+// - `n_threads` threads for the `compute_pool` (tensor math is divided among compute workers)
 // - 1 thread for the `write_worker`
 //
 struct scheduler {
     const int32_t n_threads;
 
-    // metadata for all tensors in the model
+    // per-tensor metadata for all tensors in the model
     std::vector<tensor_sched_data> tschd_vec;
+
+    size_t largest_tensor_size_src = 0; // size of largest tensor to be quantized (as src type)
+    size_t largest_tensor_size_f32 = 0; // size of largest tensor to be quantized (as f32)
+    size_t largest_tensor_size_dst = 0; // size of largest tensor to be quantized (as dst type)
 
     //
     // scheduling pipeline buffers (one of each at most)
     //
 
-    // don't need this if using mmap
-    std::vector<uint8_t> buf_read; // size = largest tensor (as found) (`largest_tensor_size`)
+    // size: largest_tensor_size_src
+    std::vector<uint8_t> buf_read; // don't need this if using mmap?
 
-    // dequantization compute buffer
-    std::vector<float> buf_dequant; // size = largest tensor (as f32) (`largest_tensor_size_dequant`)
+    // size: largest_tensor_size_f32
+    std::vector<float> buf_dequant; // dequantization buffer
 
-    // quantization compute buffer
-    std::vector<uint8_t> buf_quant; // size = largest tensor (quantized) (`largest_tensor_size_quant`)
+    // size: largest_tensor_size_dst
+    std::vector<uint8_t> buf_quant; // quantization buffer (do we really need this?)
 
-    // hold tensor data (NOTE: tensors must be in order in the output file)
-    std::vector<uint8_t> buf_write; // size = largest tensor (quantized)
-
-    size_t largest_tensor_size         = 0;
-    size_t largest_tensor_size_dequant = 0;
-    size_t largest_tensor_size_quant   = 0;
+    // size = largest tensor (as dst type)
+    std::vector<uint8_t> buf_write; // hold tensor data for writing (NOTE: tensors must be in order in the output file)
 
     compute_pool pool;
 
@@ -95,7 +87,7 @@ struct scheduler {
     scheduler(const int32_t _n_threads, std::vector<tensor_sched_data> _tschd_vec):
         n_threads(_n_threads), tschd_vec(_tschd_vec), pool(_n_threads)
     {
-        for (int32_t idx; idx < tschd_vec.size(); idx++) {
+        for (int32_t idx = 0; idx < tschd_vec.size(); idx++) {
         /*
             TODO: set these:
             largest_tensor_size         = ...;
@@ -104,13 +96,25 @@ struct scheduler {
         */
         }
 
-        // TODO: reserve pipeline buffers
+        // TODO: allocate pipeline buffers
     };
 
-    void run() {
+    ~scheduler() {
+        stop();
+    }
+
+    void start() {
         // TODO: start `read_worker` thread
         // TODO: THIS thread should manage the compute pool
         // TODO: start `write_worker` thread
         // return void when done, throw std::runtime_error if something fails
+    }
+
+    void stop() {
+        // TODO: graceful shutdown + deallocation of buffers
+    }
+
+    void submit_compute(tensor_sched_data & tschd) {
+        // TODO: 
     }
 };
--- src/llama-quant.h
+++ src/llama-quant.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 
-// store result of parsing --tensor-type option
+// result of parsing --tensor-type option
 struct tensor_type_option {
     std::string name;
     ggml_type   type = GGML_TYPE_COUNT;
@@ -24,13 +24,14 @@ enum class tensor_category {
     OTHER
 };
 
-// per-tensor info needed by the quantization work scheduler for efficient quantization.
+// per-tensor info needed by the quantization work scheduler.
 // constructed in llama-quant.cpp, passed to llama-quant-scheduler.cpp, not used otherwise.
 struct tensor_sched_data {
-    const int64_t ne0; // ncols
-    const int64_t ne1; // nrows
-    const int64_t ne2; // n_expert (or any other 3rd dimension)
-    const int64_t ne3; // 4D (currently unused)
-    const ggml_type src_type;
-    const ggml_type dst_type;
+    const void * const src_data; // pointer to raw source tensor data, read-only
+    const ggml_type    src_type;
+    const ggml_type    dst_type;
+    const int64_t      ne0; // ncols
+    const int64_t      ne1; // nrows
+    const int64_t      ne2; // n_expert (or any 3rd tensor dimension)
+    const int64_t      ne3; // any 4th tensor dimension (currently unused, always 1)
 };

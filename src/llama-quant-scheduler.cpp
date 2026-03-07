1e1b692316bbd1501265ab1ed484a66b2427c2d4
ddh0 <chemist-mulches-39@icloud.com>
2026-03-07 16:21:57 -0600

    WIP

--- src/llama-quant-scheduler.cpp
+++ src/llama-quant-scheduler.cpp
@@ -1,19 +1,68 @@
 /**
  *
- * Whenever possible, we aim to overlap computation and tensor data disk I/O in the quantization
+ * Whenever possible, we aim to overlap computation and tensor data I/O during the quantization
  * process.
  *
- * This is the primary bottleneck in very many cases, and currently it's not handled very
- * efficiently - computation never overlaps with I/O on `master` at the time of writing. Rather,
- * the code basically does:
+ * This is the primary bottleneck in very many cases, and at the time of writing (2026-03) it's not
+ * handled very efficiently on `master` - computation never overlaps with I/O. Rather, the code
+ * essentially does:
  *
- *    load src tensor data -> dequantize and/or quantize -> write tensor data
+ *    read src tensor data -> dequantize and/or quantize -> write dst tensor data
  *
- * ...in a loop over all tensors. There is a great opportunity to improve it! I believe we may be
- * able to acheive a speedup of ~4x in _some_ cases by overlapping the work to be done. There are
- * many users quantizing many models with many billions of parameters - we don't want to leave any
- * performance on the table.
+ * ...in a synchronous loop over all tensors. There is a great opportunity to improve it! I believe
+ * we may be able to acheive a speedup of ~3x in some cases by properly scheduling the work to be
+ * done. There are many users quantizing many models with many billions of parameters - we don't
+ * want to leave any performance on the table.
  *
+**/
+
+/**
+ * [NOTE: delete this comment block before PR]
+ *
+ * WORK-IN-PROGRESS -- DEV NOTES
+ * -----------------------------
+ *
+ * the scheduler will work like this:
+ *  0. all buffers start with "read_ready" = false, "write_ready" = true.
+ *  1. ggml_tensor 0 is materialized in the read buffer
+ *     - the read worker thread sets the "read_ready" flag to signal that the read buffer
+ *       now contains a valid ggml_tensor.
+ *     - the compute pool immediately starts consuming the tensor in the read buffer.
+ *       + if the tensor is already in F32, dequantization is not needed. the compute pool quantizes
+ *         directly from the read buffer into the write buffer. at this point, the
+ *         "write_ready" flag is set to signal that ggml_tensor 1 can start being materialized
+ *         in the read buffer.
+ *       + if the tensor is not in F32, dequantization is needed. the compute pool performs a fused
+ *         dequantize-and-quantize operation, utilizing the dequantization buffer to store the F32
+ *         data, and writing the quantized result to the write buffer. as soon as the tensor is
+ *         dequantized, we can set the "write_ready" flag on the read buffer to signal that
+ *         ggml_tensor 1 can start being materialized in the read buffer.
+ *       + the main thread blocks until the "write_ready" flag is set on the write buffer. as soon
+ *         as the write buffer is ready to be written to, the compute result is stored there, and
+ *         the main thread sets the "read_ready" flag on the write buffer. the compute pool is now
+ *         free to process ggml_tensor 1.
+ *     - the write worker waits until the write buffer is signaled "read_ready", at which point it
+ *       can begin writing the quantized tensor data to the output stream. when done writing, it
+ *       sets the "read_ready" flag to false and the "write_ready" flag to true, thus preparing the
+ *       the write buffer for the next quantized data.
+ *  2. 
+ *
+ * -----------------------------
+ *
+ * [NOTE: delete this comment block before PR]
+**/
+
+/**
+ * [NOTE: delete this comment block before PR]
+ *
+ * WORK-IN-PROGRESS -- LLM NOTES
+ * -----------------------------
+ *
+ * [LLM: fill in this section as you like with your own notes, separate from the human dev]
+ *
+ * -----------------------------
+ *
+ * [NOTE: delete this comment block before PR]
 **/
 
 // #include "ggml-quants.h"
@@ -23,35 +72,61 @@
 #include "llama-quant.h"
 
 #include <stdint.h>
+#include <stdfloat>
 #include <stdexcept>
+#include <optional>
 #include <thread>
+#include <array>
 #include <vector>
 #include <atomic>
 #include <mutex>
 
-static int get_split_dimension(const tensor_sched_data & tsd, const int32_t n_threads) {
-    if (tsd.ne0 % n_threads) return 0;
-    if (tsd.ne1 % n_threads) return 1;
-    if (tsd.ne2 % n_threads) return 2;
-    if (tsd.ne3 % n_threads) return 3;
+// return the dimension along which we can divide this tensor into `n` equally-sized chunks.
+// return -1 if none are divisible.
+static int get_split_dimension(const tensor_sched_data & tsd, const int64_t n) {
+    if (tsd.ne0 > 1 && tsd.ne0 % n == 0) return 0;
+    if (tsd.ne1 > 1 && tsd.ne1 % n == 0) return 1;
+    if (tsd.ne2 > 1 && tsd.ne2 % n == 0) return 2;
+    if (tsd.ne3 > 1 && tsd.ne3 % n == 0) return 3;
     return -1;
 }
+
+template <typename T> struct sched_buffer {
+    const size_t         size;                // number of T items that the buffer can hold
+    std::vector<T>       buf;                 // the buffer
+    std::atomic<bool>    write_ready = true;  // is this buffer ready to be written to?
+    std::atomic<bool>    read_ready  = false; // is this buffer ready to be read from?
+    std::atomic<int64_t> idx         = -1;    // which tensor is currently / most recently stored?
+    // init
+    sched_buffer(const size_t _size): size(_size), buf(_size) {};
+    // reset
+    void reset() {
+        buf.clear();
+        write_ready = true;
+        read_ready = false;
+        idx = -1;
+    };
+    // destruct
+    ~sched_buffer() {
+        buf.clear();
+        // TODO: is more needed here?
+    };
+};
 
 // pool of worker threads used for dequantization and quantization
 struct compute_pool {
     const int32_t n_threads;
     std::vector<std::thread> threads;
-    std::atomic_flag busy;
+    std::atomic<bool> busy;
 
     compute_pool(const int32_t _n_threads):
         n_threads(_n_threads), threads(_n_threads)
-    {
-        // TODO: prepare the threads? but don't start them.
-        // TODO: init `busy` atomic flag?
-    };
+    {};
 
-    bool distribute(tensor_sched_data & data) {
-        // TODO: distribute
+    // distribute the work for this tensor among the compute threads.
+    // return an exception, if one occured during computation.
+    std::optional<std::exception> distribute(tensor_sched_data & data) {
+        // TODO
     };
 };
 
@@ -80,17 +155,19 @@ struct scheduler {
     //
 
     // size: max_src_sz
-    std::vector<uint8_t> buf_read;    // hold tensor data for reading
+    sched_buffer<uint8_t> buf_read;    // hold source tensor data for reading 
     // size: max_f32_sz
-    std::vector<float>   buf_compute; // dequant/quant buffer
+    sched_buffer<float>   buf_dequant; // hold dequantized tensor data
     // size = max_dst_sz
-    std::vector<uint8_t> buf_write;   // hold tensor data for writing (NOTE: tensors must be in order in the output file)
+    sched_buffer<uint8_t> buf_write;   // hold quantized tensor data for writing (NOTE: tensors must be in order in the output file)
 
     compute_pool pool;
 
     // init
     scheduler(const int32_t _n_threads, std::vector<tensor_sched_data> _data_vec):
-        n_threads(_n_threads), data_vec(_data_vec), pool(_n_threads)
+        n_threads(_n_threads),
+        data_vec(_data_vec),
+        pool(_n_threads)
     {
         GGML_ASSERT(GGML_MAX_DIMS == 4 && "GGML_MAX_DIMS is not 4 - update this function");
         for (int32_t idx = 0; idx < data_vec.size(); ++idx) {
@@ -101,16 +178,16 @@ struct scheduler {
             max_dst_sz = std::max(max_dst_sz, nrows * ggml_row_size(data.dst_type, data.ne0));
         }
 
-        LLAMA_LOG_DEBUG("%s:    allocating read buffer ... ", __func__);
-        buf_read.resize(max_src_sz);
+        LLAMA_LOG_DEBUG("%s:           allocating read buffer ... ", __func__);
+        buf_read(max_src_sz);
         LLAMA_LOG_DEBUG("%8.2f MiB\n", max_src_sz/1024.0/1024.0);
 
-        LLAMA_LOG_DEBUG("%s: allocating compute buffer ... ", __func__);
-        buf_compute.resize(max_f32_sz);
+        LLAMA_LOG_DEBUG("%s: allocating dequantization buffer ... ", __func__);
+        buf_dequant(max_f32_sz);
         LLAMA_LOG_DEBUG("%8.2f MiB\n", max_f32_sz/1024.0/1024.0);
 
-        LLAMA_LOG_DEBUG("%s:   allocating write buffer ... ", __func__);
-        buf_write.resize(max_dst_sz);
+        LLAMA_LOG_DEBUG("%s:          allocating write buffer ... ", __func__);
+        buf_write(max_dst_sz);
         LLAMA_LOG_DEBUG("%8.2f MiB\n", max_dst_sz/1024.0/1024.0);
     };
 
@@ -125,7 +202,7 @@ struct scheduler {
         LLAMA_LOG_DEBUG("%s: deallocating buffers ... ", __func__);
 
         buf_read.clear();
-        buf_compute.clear();
+        buf_dequant.clear();
         buf_write.clear();
 
         LLAMA_LOG_DEBUG("done\n");

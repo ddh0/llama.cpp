16bef330b9d0f1a53051263765702ce47f4302bb
ddh0 <dylanhalladay02@icloud.com>
2026-03-25 02:59:31 -0500

    begin to adopt command pattern

--- src/llama-quant.h
+++ src/llama-quant.h
@@ -1,21 +1,25 @@
 #pragma once
 
-// per-tensor info needed by the quantization work scheduler.
-// constructed in llama-quant.cpp, passed to llama-quant-scheduler.cpp, not used otherwise.
-struct tensor_sched_data {
-    const std::vector<int64_t> ne;
-    const ggml_type src_type;
-    const ggml_type dst_type;
-    const void * src_data; // pointer to raw source tensor data buffer, read-only
-    const void * imatrix;  // pointer to imatrix data, or nullptr, read-only
-    tensor_sched_data(
-        const std::vector<int64_t> _ne,
-        const ggml_type _src_type,
-        const ggml_type _dst_type,
-        const void * _src_data,
-        const void * _imatrix
-    )
-    {
-        
-    }
+enum sched_cmd_status {
+    CMD_STATUS_PENDING,
+    CMD_STATUS_IN_PROGRESS,
+    CMD_STATUS_COMPLETE,
+    CMD_STATUS_COUNT, // always last
+};
+
+// types of operations that performed the quantization work scheduler
+enum sched_cmd_type {
+    CMD_TYPE_READ,
+    CMD_TYPE_DEQUANTIZE,
+    CMD_TYPE_QUANTIZE,
+    CMD_TYPE_WRITE,
+    CMD_TYPE_COUNT // always last
+};
+
+// unit of work for the quantization work scheduler (command pattern)
+struct sched_cmd {
+    const ggml_tensor * tensor;
+    const enum sched_cmd_type sched_cmd_type;
+
+    std::atomic<enum sched_cmd_status> sched_cmd_status;
 };

afe111b744224f1c5caa4b5e42a8bdbde6866860
ddh0 <chemist-mulches-39@icloud.com>
2026-04-25 00:10:48 -0500

    nit

--- tools/tensor-capture-numpy/tensor-capture-numpy.cpp
+++ tools/tensor-capture-numpy/tensor-capture-numpy.cpp
@@ -6,9 +6,9 @@
  * of the hidden state `cur`, or any other node in the graph.
  *
  * The purpose of saving tensors in NumPy format rather than binary is to make debugging and
- * analysis easier - we can capture multiple runs from different models and compare them using a
- * simple Python script, for example. Once a tensor is saved to disk as a `.npy` file, then it
- * can be directly as a NumPy array in any Python script.
+ * analysis easier - for example, we can capture multiple runs from different models and compare
+ * them using a simple Python script. Once a tensor is saved to disk as a `.npy` file, then it
+ * can be loaded directly as a NumPy array in any Python script using `np.load`.
  *
  * For more information, see the README in this directory, as well as the Python script.
  *

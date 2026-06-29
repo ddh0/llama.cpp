8468d092c1eaa307bded58e882efb1569848efc5
ddh0 <dylanhalladay02@icloud.com>
2026-06-29 15:08:30 -0500

    initial commit for branch

---
+++ tools/playground/playground.cpp
@@ -0,0 +1,15 @@
+// ================================================================================================
+// playground.cpp
+// ================================================================================================
+//
+// this file is not meant to be incorporated into llama.cpp!
+//
+// it is only meant as a development and testing environment.
+//
+// ================================================================================================
+
+#include "playground.h"
+
+int main() {
+    std::setlocale(LC_NUMERIC, "C");
+}
---
+++ tools/playground/playground.h
@@ -0,0 +1,18 @@
+// ================================================================================================
+// playground.h
+// ================================================================================================
+//
+// this file is not meant to be incorporated into llama.cpp!
+//
+// it is only meant as a development and testing environment.
+//
+// ================================================================================================
+
+#pragma once
+
+#include "llama.h"
+#include "log.h"
+#include "llama-model.h"
+#include "llama-model-loader.h"
+#include "common.h"
+#include "gguf.h"
